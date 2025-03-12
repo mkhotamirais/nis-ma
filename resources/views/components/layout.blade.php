dd1eedd332942968c31e9f5a8fa663412dc26fd7
mkhotamirais <mkhotamirais@gmail.com>
2025-03-12 19:49:15 +0700

    add tag

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,7 +17,7 @@
     <link rel="shortcut icon" href="{{ asset('storage/logo/favicon.ico') }}" type="image/x-icon">
 
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6RZ3FBFD9Q"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3N5CGPRCD5"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
 
@@ -26,7 +26,7 @@
         }
         gtag('js', new Date());
 
-        gtag('config', 'G-6RZ3FBFD9Q');
+        gtag('config', 'G-3N5CGPRCD5');
     </script>
 
     {{-- alpine --}}
